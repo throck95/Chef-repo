baa7c94bc8930607b0c9e819056f4097ffaf2bba
EC2 Default User <ec2-user@ip-172-31-53-168.ec2.internal>
2016-01-26 23:18:38 +0000

    committing webserver

---
+++ default.rb
@@ -0,0 +1,15 @@
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+
+package "httpd"
+
+file "/var/www/html/index.html" do
+	content "Hello World!"
+end
+
+service "httpd" do
+	action :start
+end
---
+++ webserver/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package "httpd"
+	content "Hello World!"
+end
+
+service "httpd" do
+	action :start
+end
